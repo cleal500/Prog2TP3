a2719a84addb37b5cdf61306e3487257bf0f4af2
lexalex123 <alexlex44@gmail.com>
2017-12-08 15:48:46 -0500

    first commit

---
+++ Principal.java
@@ -0,0 +1,12 @@
+import static javax.swing.SwingUtilities.invokeLater;
+
+public class Principal {
+
+    public static void main (String [] args){
+        invokeLater( () -> new LSystem () );
+
+
+    }
+
+
+}
